0adc2ee6c6cd6dbbfefe26efa70b8ddde2804fe6
MassimoDamiano <106933868+MassimoDamiano@users.noreply.github.com>
2023-04-30 17:29:09 -0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
 let sliderSectionLast = sliderSection[sliderSection.length -1];
@@ -41,48 +40,5 @@ btnLeft.addEventListener("click", function(){
 
 setInterval(function(){
     Next();
-=======
-const slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(".slider__section");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
-
-const  btnRight = document.querySelector("#btn__right")
-const  btnLeft = document.querySelector("#btn__left")
-
-slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-
-function Next(){
-    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
-    slider.style.marginLeft ="-200%"
-    slider.style.transition = "all 0.5s"
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement("beforeend" , sliderSectionFirst);
-        slider.style.marginLeft = "-100%"
-    },500);
-}
-
-
-function Prev(){
-    let sliderSection = document.querySelectorAll(".slider__section");
-    let sliderSectionLast = sliderSection[sliderSection.length -1];
-    slider.style.marginLeft ="0";
-    slider.style.transition = "all 0.5s"
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement("afterbegin" , sliderSectionLast);
-        slider.style.marginLeft = "-100%"
-    },500);
-}
-
-btnRight.addEventListener("click", function(){
-    Next();
-});
-btnLeft.addEventListener("click", function(){
-    Prev();
-});
-
-setInterval(function(){
-    Next();
->>>>>>> 2be045fc6eb74897e1ac3b52096810635607b5f9
+},3300);
 },3300);
