018ccd4c99c64c76a4eb219da751ac7b29a5c29f
MassimoDamiano <106933868+MassimoDamiano@users.noreply.github.com>
2023-04-30 16:45:01 -0300

    arreglando

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () => {
+    aside.classList.add("aside-visible");
+})
+
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+=======
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () => {
+    aside.classList.add("aside-visible");
+})
+
+closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+>>>>>>> 2be045fc6eb74897e1ac3b52096810635607b5f9
+})
